348478f994df6d70e226f5f3b053357ed53c4a7c
Jon Barber <jon.barber@acm.org>
2011-06-06 16:51:29 +0100

    Removed unused constructors

--- core/src/main/java/com/ninedemons/circuitbreaker/command/exception/CommandException.java
+++ core/src/main/java/com/ninedemons/circuitbreaker/command/exception/CommandException.java
@@ -9,15 +9,8 @@ public class CommandException extends Exception {
         super();
     }
 
-    public CommandException(String message) {
-        super(message);
-    }
-
     public CommandException(Throwable cause) {
         super(cause);
     }
 
-    public CommandException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
